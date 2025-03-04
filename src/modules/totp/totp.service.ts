e9f176150de4d7918370203089e6acb5150b79e3
Canmi21 <9997200@qq.com>
2025-03-05 00:21:59 +0800

    build: remove src

--- src/modules/totp/totp.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import * as speakeasy from 'speakeasy';
-
-@Injectable()
-export class TotpService {
-  generateSecret(): string {
-    return speakeasy.generateSecret({ length: 20 }).base32;
-  }
-}
--- src/types/speakeasy.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'speakeasy' {
-  export function generateSecret(options?: { length?: number; name?: string }): { base32: string; otpauth_url: string };
-  export function totp(options: { secret: string; encoding?: string }): string;
-  export function verify(options: { secret: string; token: string; encoding?: string }): boolean;
-}
