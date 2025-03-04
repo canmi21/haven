6e8ea90efb602245d44071f5c92aa6af2d8e422d
Canmi21 <9997200@qq.com>
2025-03-04 21:23:36 +0800

    add: mongodb

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import * as dotenv from 'dotenv';
+import { testMongoConnection } from './config/database.config';
+
+dotenv.config();
 
 async function bootstrap() {
+  await testMongoConnection();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 33321);
+  await app.listen(33321);
 }
+
 bootstrap();
