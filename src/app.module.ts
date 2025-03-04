59a443770baa329045e5832d6327902edd3edf9a
Canmi21 <9997200@qq.com>
2025-03-04 13:30:25 +0800

    chore: remove test file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
