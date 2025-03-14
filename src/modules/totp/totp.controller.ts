f2ac692e4e8ebe55bea42f8d3b6013e6e0d0cc6b
Canmi21 <9997200@qq.com>
2025-03-14 13:08:02 +0800

    add: totp verify

--- src/modules/totp/totp.controller.ts
+++ src/modules/totp/totp.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Query } from '@nestjs/common';
-import * as speakeasy from 'speakeasy';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common'
+import * as speakeasy from 'speakeasy'
+import { TotpService } from './totp.service'
+import { TotpGuard } from './totp.guard'
 
 @Controller('v1/auth/totp-secret')
 export class TotpController {
+  constructor(private readonly totpService: TotpService) {}
+
   @Get()
   generateTotpSecret(@Query('length') length?: number) {
-    const secretLength = Math.min(Number(length) || 32, 256);
+    const secretLength = Math.min(Number(length) || 32, 256)
     return {
       secret: speakeasy.generateSecret({ length: secretLength }).base32,
-    };
+    }
   }
+
+  @Get('verify')
+  @UseGuards(TotpGuard)
+  verifyTotp() {
+    return {
+      valid: true,
+      message: 'Verification successful, but no further actions are provided.',
+    }
+  }  
 }
