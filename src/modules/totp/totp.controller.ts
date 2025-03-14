ad4de56d38b5c23dffdecac19b89ee61d605ea94
Canmi21 <9997200@qq.com>
2025-03-14 19:18:53 +0800

    fix: succ response

--- src/modules/totp/totp.controller.ts
+++ src/modules/totp/totp.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query, UseGuards } from '@nestjs/common'
 import * as speakeasy from 'speakeasy'
 import { TotpService } from './totp.service'
 import { TotpGuard } from './totp.guard'
+import { successResponse } from '../../utils/response'
 
 @Controller('v1/auth/totp-secret')
 export class TotpController {
@@ -10,17 +11,17 @@ export class TotpController {
   @Get()
   generateTotpSecret(@Query('length') length?: number) {
     const secretLength = Math.min(Number(length) || 32, 256)
-    return {
+    return successResponse({
       secret: speakeasy.generateSecret({ length: secretLength }).base32,
-    }
+    })
   }
 
   @Get('verify')
   @UseGuards(TotpGuard)
   verifyTotp() {
-    return {
-      valid: true,
-      message: 'Verification successful, but no further actions are provided.',
-    }
-  }  
+    return successResponse(
+      true,
+      'Verification successful, but no further actions are provided.'
+    )
+  }
 }
